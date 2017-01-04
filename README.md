c1255387b671657439699b3533b970b9b4dc83f7
David Parsons <djp424@gmail.com>
2017-01-04 13:36:32 -0500

    python base

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Creator
 A program that creates programs
+
+# Timer
+Another thing the base template programs do is give you a good way to time your code.
---
+++ templates/base.py
@@ -0,0 +1,31 @@
+import time
+import os.path
+
+# timer for speed testing of program logic
+def run_timer():
+	tests = 30
+	start_time = time.time() # start
+	for i in xrange(tests):
+		run_tests()
+	end_time = time.time()
+	total_time = (end_time - start_time) / tests # end
+	print("Runtime: %.10f seconds" % total_time)
+	# save results
+	file_name = "speed.csv"
+	if os.path.isfile(file_name) is False:
+		with open(file_name, "a") as results:
+			results.write("time,tests\n")
+	with open(file_name, "a") as results:
+		results.write("%.10f" % total_time)
+		results.write(",%s\n" % tests)
+
+# program logic
+def base(the_input):
+	return the_input
+
+# test cases
+def run_tests():
+	print base('hello world')
+
+run_tests()
+run_timer()
